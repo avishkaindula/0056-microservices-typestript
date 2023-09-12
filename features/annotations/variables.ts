47091705f3b01623b3bf46402f4752556eb464c6
Avishka Indula <avishkainduladev@gmail.com>
2023-09-12 16:21:08 +0530

    When to use type annotations

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,4 +1,6 @@
-let apples: number = 5;
+let apples = 5;
+// We don't need to specify the type of the variable when we initialize it in the same line
+// It's called type inference
 let speed: string = "fast";
 let hasName: boolean = true;
 
@@ -27,3 +29,31 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json); // any type
+console.log(coordinates); // {x: 10, y: 20}
+
+// 2) When we declare a variable on one line and initialize it later
+let words = ["red", "green", "blue"];
+let foundWord: boolean; // any type
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
+
+// 3) When we want a variable to have a type that can't be inferred
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false; // union type
+// numberAboveZero can be a boolean or a number. It has a boolean value by default. But it can be a  
+// number later if the condition is true
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
